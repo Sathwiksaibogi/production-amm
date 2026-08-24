1d43882b32cf9c808feb893db2e285d439b94983
Sathwiksaibogi <sathwiksaibogi05@gmail.com>
2026-08-24 14:30:10 +0000

    feat(math):updated lib.rs

--- amm-math/src/lib.rs
+++ amm-math/src/lib.rs
@@ -3,3 +3,10 @@ mod fees;
 mod integer_sqrt;
 mod liquidity;
 mod swap;
+
+pub use error::AmmMathError;
+pub use liquidity::{
+    LiquidityWithdrawal, calculate_initial_liquidity, calculate_liquidity_added,
+    calculate_liquidity_withdrawal,
+};
+pub use swap::{SwapResult, calculate_swap_output};
